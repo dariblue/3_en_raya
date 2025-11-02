ba770c46064bb443005fa7ccbf7c0d53c9517ed6
Nia Andrade <nia.andrade.rd@gmail.com>
2025-11-02 13:22:26 +0100

    cambio comentarios / limpierza inicializar.asm

--- inicializar.asm
+++ inicializar.asm
@@ -1,26 +1,27 @@
-;Código del estudiante
+;Codigo previo al juego que inicializa jugadores y llama a la rutina de juego
 Inicializar:
-    CALL inic_jugadores    ; establecer jugadores (DE = activo, DE' = otro)
+    CALL inic_jugadores    ; Inicializar jugadores
     CALL Juego             ; Llamar a la rutina de juego
     RET
 
 ; Inicializa jugadores con valores:
-; En D = color ficha jugador activo, E = nº jugador activo
-; En D'/E' = color y nº del otro jugador (registros primados)
+; En D = color ficha jugad or activo, E = nº jugador activo (primarios)
+; En D'/E' = color y nº del otro jugador (secundarios)
 inic_jugadores:
-    ; Jugador activo (primarios): J1 = ROJO, nº 1
-    LD D, 2      ; color ficha jugador1 = 2 (rojo)
+    ; Jugador activo (primario) J1 = ROJO, nº 1
+    LD D, 2      ; color ficha J1 = 2 (rojo)
     LD E, 1      ; nº jugador1 = 1
 
-    EXX          ; pasar a registros primados (trabajamos en DE' como DE)
-    ; Otro jugador (primados): J2 = AMARILLO, nº 2
-    LD D, 6      ; color ficha jugador2 = 6 (amarillo)
+    EXX          ; cambio del registro primario a secundario
+
+    ; Otro jugador (secundario): J2 = AMARILLO, nº 2
+    LD D, 6      ; color ficha J2 = 6 (amarillo)
     LD E, 2      ; nº jugador2 = 2
 
-    ;EXX          ; volver a registros primarios: DE contiene J1, DE' contiene J2
+    EXX          ; volver a registros primarios: DE contiene J1, (DE)' contiene J2
     RET
 
-; Cambia jugador activo <-> otro usando EXX (muy barato)
+; Cambia el jugador activo (primario <-> secundario)
 cambiar_jugapuertas:
     EXX
     RET
