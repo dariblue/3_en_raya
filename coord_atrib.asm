991fec52a3ca512606fe13161c81fea7fee5b207
Nia Andrade <nia.andrade.rd@gmail.com>
2025-11-21 10:27:34 +0100

    CoordAtrib Actualizado

--- coord_atrib.asm
+++ coord_atrib.asm
@@ -1,3 +1,13 @@
 ; Rutina que recibe en B,C las coordenadas de la pantalla (fila, columna)
 coord_Atrib:
+    PUSH AF
+    LD A, H
+    SLA A: SLA A: SLA A: SLA A   ; A = fila * 16
+    OR L
+    LD L, A
+    LD H, A
+    SRA A: SRA A: SRA A  ; A = fila * 4
+    OR $58
+    LD H, A 
+    POP AF
     RET
