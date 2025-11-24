2e828688409dda76c020594a07edff47adfe7e0c
Dario Rodríguez <72631983+dariblue@users.noreply.github.com>
2025-11-24 13:04:07 +0100

    ayuda, no explotes

--- conecta4_juego.asm
+++ conecta4_juego.asm
@@ -11,9 +11,12 @@ drawBoard:
     LD BC, $5B00 - $4000    ; tamaño: $5B00 - $4000 = $1B00 (6912 bytes) tamaño pantalla ZX Spectrum
     LDIR 
     
-    ; Pintar ficha inicial del jugador activo usando overlay full-screen
-    LD A, 5*8
-    CALL piececita
+    ; Pintar ficha inicial del jugador activo encima de la primera columna
+    ; Coordenadas: Fila 0, Columna 2 (Tablero empieza en 2,2)
+    LD H, 0             ; Fila 0
+    LD L, 2             ; Columna 2
+    LD D, $10           ; Color Rojo (Paper 2 = 010 -> 00 010 000 = $10)
+    CALL dibujar_ficha
     RET
 
 ; Coord_Atrib: ; Copiado del de bienvenida.asm ;TODO: pasar a mannin.asm
