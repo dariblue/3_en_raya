883205cd67a0203de59ff796f7d79c066b8e0a49
Nia Andrade <nia.andrade.rd@gmail.com>
2025-12-31 01:58:31 +0100

    carita xd

--- pantalla_final.asm
+++ pantalla_final.asm
@@ -10,6 +10,12 @@ despedida:
     LD A, 0         ; Inicializar el registro A con 0
     OUT ($FE), A    ; Pone el borde de la pantalla en negro
 
+    ; Pintar la pantalla de bienvenida
+    LD HL, CARITA       ; Dirección del bucle que pinta la pantalla
+    LD DE, $4000    
+    LD BC, $5B00 - $4000 
+    LDIR       
+
     ; Título de despedida
     LD A,3+$80      ; Letra moradita, fondo negro, parpadeante
     LD B,1          ; Coordenadas para pintar el título
@@ -26,6 +32,7 @@ despedida:
 
     RET
 
+CARITA: INCBIN "carita.scr"
 Titulo_final:    db "Partida epiquisima bro", 0    ; Título
 Mensaje_final:   db "Quieres jugar de nuevo (S/N)? ",0   ; Mensaje inicial
 Adios_final:     db "Gracias por jugar. Adios!",0 ; Mensaje de despedida
